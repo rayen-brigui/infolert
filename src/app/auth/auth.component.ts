85b2960e5bee332059601f5df7591f5fee660dcd
Rayen Brigui <rayenbrigui92@outlook.fr>
2023-08-21 16:13:42 +0100

    update auth.component.html, auth.component.scss, auth.component.ts and auth.guard.ts

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -10,22 +15,30 @@ import { FormsModule } from '@angular/forms';
   imports: [IonicModule, FormsModule, CommonModule],
 })
 export class AuthComponent implements OnInit {
-  constructor() {}
-
+  constructor(private navCtrl: NavController) {}
   ngOnInit() {}
   loginEmail: string = '';
   loginPassword: string = '';
 
   // Sign-up form variables
+  signupFname: string = '';
+  signupLname: string = '';
   signupEmail: string = '';
   signupPassword: string = '';
+  signupConfirmPassword: string = '';
 
   // Show/hide sign-up form
   showSignUpForm: boolean = false;
-
-
+  route = new Router();
   // Login method
   login() {
+    if (this.loginEmail !== '' || this.loginPassword !== '') {
+      this.navCtrl.navigateForward('/');
+
+      console.log('====================================');
+      console.log('suppose temchi');
+      console.log('====================================');
+    }
     // Implement your login logic here
     // You can use this.loginEmail and this.loginPassword to access the user's input
   }
